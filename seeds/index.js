867d4d710313a99bd823a88d46f59ef10155d11c
Josh <joshuaelitate@gmail.com>
2023-02-06 21:19:34 -0800

    commented

--- seeds/index.js
+++ seeds/index.js
@@ -1,13 +1,13 @@
 const sequelize = require('../config/connection');
-const seedGallery = require('./galleryData');
-const seedPaintings = require('./paintingData');
+// const seedGallery = require('./galleryData');
+// const seedPaintings = require('./paintingData');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  await seedGallery();
+  // await seedGallery();
 
-  await seedPaintings();
+  // await seedPaintings();
 
   process.exit(0);
 };
