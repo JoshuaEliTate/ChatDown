0b6bdd927c5db6823b94742a20e989432239013b
Jason Chen <jason.ccfu@gmail.com>
2023-02-07 20:19:23 -0800

    new models

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+
+console.log(process.env.DB_NAME);
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
+
   {
-    host: 'localhost',
-    dialect: 'mysql',
+    host: "127.0.0.1",
+    dialect: "mysql",
     port: 3306,
+    dialectOptions: {
+      socketPath: "/tmp/mysql.sock",
+    },
   }
 );
 
