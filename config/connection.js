92a013588b3090b67cd58a43be9aff91f0261059
Josh <joshuaelitate@gmail.com>
2023-02-09 16:06:48 -0800

    connection test

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,10 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: '127.0.0.1',
+    host: 'localhost',
     dialect: 'mysql',
     port: 3306,
-    dialectOptions: {
-      socketPath: "/tmp/mysql.sock"
   }
-});
+);
 
 module.exports = sequelize;
