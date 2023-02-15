dce21bf53ff781e62d73ed1ad9065cfb6f5e08c0
Jason Chen <jason.ccfu@gmail.com>
2023-02-14 21:26:18 -0800

    fix location

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,30 +1,28 @@
 const router = require("express").Router();
 
-const Joi = require('joi');
+const Joi = require("joi");
 
-const { PostComment} = require("../../models");
-
+const { PostComment } = require("../../models");
 
 const schema = Joi.object({
   message: Joi.string().trim(),
-  city: Joi.string()
+  city: Joi.string(),
 });
 
 // CREATE new post
 router.post("/", async (req, res) => {
-
-  console.log('prevalidation', req.body.message)
-  const {error, value} = schema.validate(req.body);
+  console.log("prevalidation", req.body.message);
+  const { error, value } = schema.validate(req.body);
   if (error) {
-    const {message} = error.details[0];
-    console.log(error.details)
-    return res.status(400).json({error: message});
+    const { message } = error.details[0];
+    console.log(error.details);
+    return res.status(400).json({ error: message });
   }
 
-  console.log('postvalidation', req.body.message)
+  console.log("postvalidation", req.body.message);
 
-  console.log(req.body.message)
-  console.log(req.body.city)
+  console.log(req.body.message);
+  console.log(req.body.city);
 
   try {
     console.log(req.body.city);
@@ -45,21 +43,21 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get("/", async (req, res) => {
-  try {
-    const postComment = await PostComment.findAll();
+// router.get("/", async (req, res) => {
+//   try {
+//     const postComment = await PostComment.findAll();
 
-    const getData = postComment.map((postComment) =>
-      getData.get({ plain: true })
-    );
-    res.render("homepage", {
-      postComment,
-      loggedIn: req.session.loggedIn,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+//     const getData = postComment.map((postComment) =>
+//       getData.get({ plain: true })
+//     );
+//     res.render("homepage", {
+//       postComment,
+//       loggedIn: req.session.loggedIn,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
 
 module.exports = router;
