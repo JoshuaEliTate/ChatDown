cf27af35c200a2dd6f97e13cbf52200f65b6d075
Josh <joshuaelitate@gmail.com>
2023-02-16 18:44:02 -0800

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,36 @@
+// const Sequelize = require("sequelize");
+
+// console.log(process.env.DB_NAME);
+
+// const sequelize = new Sequelize(
+//   {
+
+//     database: process.env.DB_NAME,
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306,
+//   //   dialectOptions: {
+//   //     socketPath: "/tmp/mysql.sock",
+//   //   },
+//   }
+// );
 const Sequelize = require("sequelize");
 
-console.log(process.env.DB_NAME);
-
-const sequelize = new Sequelize(
-  {
-
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  //   dialectOptions: {
-  //     socketPath: "/tmp/mysql.sock",
-  //   },
-  }
-);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
+}
 
 module.exports = sequelize;
